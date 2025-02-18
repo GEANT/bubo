675ee616d7536142e845984d6befcbb23340ca44
Mohammadreza Sarayloo <mohammad.sarayloo@geant.org>
2025-02-18 16:05:53 +0100

    Add f string to logger format

--- core/custom_logger/logger.py
+++ core/custom_logger/logger.py
@@ -7,7 +7,7 @@ class CustomFormatter(logging.Formatter):
     red = "\x1b[31;20m"
     bold_red = "\x1b[31;1m"
     reset = "\x1b[0m"
-    format = f"\x1b[1m%(asctime)s [%(levelname)s]\x1b[0m - %(name)s - %(message)s"
+    format = "\x1b[1m%(asctime)s [%(levelname)s]\x1b[0m - %(name)s - %(message)s"
 
     FORMATS = {
         logging.DEBUG: grey + format + reset,
