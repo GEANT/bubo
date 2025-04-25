5be79d183ed8ea091b85980e25c6aa44b818a57b
Mohammadreza Sarayloo <mohammad.sarayloo@geant.org>
2025-04-25 10:15:17 +0200

    Adding modals placeholders js code

---
+++ templates/js/modals/modal_placeholders.js
@@ -0,0 +1,62 @@
+// modal_placeholders.js - Placeholder implementations for other modal types
+// These can be moved to their own files when implementing them fully
+
+/**
+ * Create HTML for DNSSEC modal
+ * @param {string} modalId - ID for the modal
+ * @param {string} domain - Domain name
+ * @param {Object} data - DNSSEC validation data
+ * @returns {string} - Modal HTML
+ */
+function createDNSSECModal(modalId, domain, data) {
+    return `
+    <div id="${modalId}" class="modal">
+        <div class="modal-content">
+            <span class="close-modal" onclick="closeModal('${modalId}')">&times;</span>
+            <h3>DNSSEC Details - ${domain}</h3>
+            <div class="results-container">
+                <pre>${JSON.stringify(data, null, 2)}</pre>
+            </div>
+        </div>
+    </div>`;
+}
+
+/**
+ * Create HTML for Email Security modal
+ * @param {string} modalId - ID for the modal
+ * @param {string} domain - Domain name
+ * @param {Object} data - Email Security validation data
+ * @returns {string} - Modal HTML
+ */
+function createEmailSecurityModal(modalId, domain, data) {
+    return `
+    <div id="${modalId}" class="modal">
+        <div class="modal-content">
+            <span class="close-modal" onclick="closeModal('${modalId}')">&times;</span>
+            <h3>Email Security Details - ${domain}</h3>
+            <div class="results-container">
+                <pre>${JSON.stringify(data, null, 2)}</pre>
+            </div>
+        </div>
+    </div>`;
+}
+
+/**
+ * Create HTML for Web Security modal
+ * @param {string} modalId - ID for the modal
+ * @param {string} domain - Domain name
+ * @param {Object} data - Web Security validation data
+ * @returns {string} - Modal HTML
+ */
+function createWebSecurityModal(modalId, domain, data) {
+    return `
+    <div id="${modalId}" class="modal">
+        <div class="modal-content">
+            <span class="close-modal" onclick="closeModal('${modalId}')">&times;</span>
+            <h3>Web Security Details - ${domain}</h3>
+            <div class="results-container">
+                <pre>${JSON.stringify(data, null, 2)}</pre>
+            </div>
+        </div>
+    </div>`;
+}
