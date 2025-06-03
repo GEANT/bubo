50220d030b56dfda402057834f62c298d3965322
Mohammadreza Sarayloo <mohammad.sarayloo@geant.org>
2025-06-03 11:52:04 +0200

    Adding main.py to call bubo

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Main entry point for the compliance-scanning tool.
+This script serves as a wrapper around the bubo package's main function.
+"""
+
+from bubo.main import main
+
+if __name__ == "__main__":
+    main()
